2bba0ac81996030aa07f58025fb2a2665ef14d6f
Bgabri <gberardi2003@gmail.com>
2023-01-01 16:15:46 +0100

    Revert "day 16"
    This reverts commit a57a4f03e167d8901b254b7b01aa5a4fa74658da.

--- Day00.hx
+++ Day00.hx
@@ -2,21 +2,20 @@ using StringTools;
 // haxe --interp --main Dayxx.hx
 
 class DayXX {
-
-    public function new() {
+    static function main() {
         trace("solution to part 1: " + part1(loadFile("inputs/XX.txt")));
         trace("solution to part 2: " + part2(loadFile("inputs/XX.txt")));    
     }
 
-    function part1(input:Array<Array<Int>>):Int {
+    static function part1(input:Array<Array<Int>>):Int {
         trace(input);
         return 0;
     }
-    function part2(input:Array<Array<Int>>):Int {
+    static function part2(input:Array<Array<Int>>):Int {
         return 0;
     }
 
-    function loadFile(file:String):Array<Array<Int>> {
+    static function loadFile(file:String):Array<Array<Int>> {
         var input:Array<Array<Int>> = [];
         var iterator = sys.io.File.read(file, false);
         
@@ -29,6 +28,4 @@ class DayXX {
         iterator.close();
         return input;
     }
-
-    static function main() new DayXX();
 }
