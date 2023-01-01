a57a4f03e167d8901b254b7b01aa5a4fa74658da
Bgabri <gberardi2003@gmail.com>
2023-01-01 16:14:34 +0100

    day 16

--- Day00.hx
+++ Day00.hx
@@ -2,20 +2,21 @@ using StringTools;
 // haxe --interp --main Dayxx.hx
 
 class DayXX {
-    static function main() {
+
+    public function new() {
         trace("solution to part 1: " + part1(loadFile("inputs/XX.txt")));
         trace("solution to part 2: " + part2(loadFile("inputs/XX.txt")));    
     }
 
-    static function part1(input:Array<Array<Int>>):Int {
+    function part1(input:Array<Array<Int>>):Int {
         trace(input);
         return 0;
     }
-    static function part2(input:Array<Array<Int>>):Int {
+    function part2(input:Array<Array<Int>>):Int {
         return 0;
     }
 
-    static function loadFile(file:String):Array<Array<Int>> {
+    function loadFile(file:String):Array<Array<Int>> {
         var input:Array<Array<Int>> = [];
         var iterator = sys.io.File.read(file, false);
         
@@ -28,4 +29,6 @@ class DayXX {
         iterator.close();
         return input;
     }
+
+    static function main() new DayXX();
 }
